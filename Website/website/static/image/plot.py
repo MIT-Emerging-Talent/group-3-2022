e67a1ee3e44a05e215a12e4a55c35fc1a05177ac
sinfido <58522642+sinfido@users.noreply.github.com>
2022-12-12 18:38:49 -0300

    Add files via upload

--- Website/website/static/image/plot.py
+++ Website/website/static/image/plot.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Nov 29 19:25:57 2022
-
 @author: ialav
 """
 
@@ -11,11 +10,11 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-pd.set_option('display.max_rows', None)
+#pd.set_option('display.max_rows', None)
 
-pd.set_option('display.max_columns', None)
-pd.set_option('display.width', 2000)
-from statsmodels.tsa.seasonal import seasonal_decompose
+#pd.set_option('display.max_columns', None)
+#pd.set_option('display.width', 2000)
+#from statsmodels.tsa.seasonal import seasonal_decompose
 
 
 def ELOP(url):
@@ -29,8 +28,7 @@ def ELOP(url):
     sns.set(font_scale=1.5, rc={'axes.facecolor':'pink','figure.facecolor':'gray'})
     sns.lineplot(x=X, y= Y, data=df, )
     sns.set(style='dark',)
-    
+    plt.savefig('ELOP')
     
 
 ELOP("https://www.ine.gub.uy/c/document_library/get_file?uuid=0c0fbfd5-d4ab-4414-96cd-dbff821e4b00&groupId=10181")
-
